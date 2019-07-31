cf909b7927c13836e62ccae8c376b475a420357e
AlpineCoder <roger.zimmermann@gmx.net>
2019-07-31 17:17:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-import sys
+import time
+import datetime
 
-print "hello world"
+while True:
+    now = datetime.datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    print date_time + " hello world"
+    time.sleep(5)
