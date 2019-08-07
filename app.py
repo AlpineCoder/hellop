3e0e1d9f6de0859f2fd3937e9a878e57296b73ad
Roger Zimmermann <roger.zimmermann@gmx.net>
2019-08-07 07:55:57 +0200

    changed to webserver

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-import time
-import datetime
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-while True:
-    now = datetime.datetime.now()
-    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-    print date_time + " hello world"
-    time.sleep(5)
+PORT_NUMBER = 8080
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+	
+	#Handler for the GET requests
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		self.wfile.write("Hello World !")
+		return
+
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+
