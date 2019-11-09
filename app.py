e9f3e82e908f397eff78e0611106e5c3ff8450b0
Roger Zimmermann <roger.zimmermann@gmx.net>
2019-11-09 10:02:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class myHandler(BaseHTTPRequestHandler):
 		self.send_header('Content-type','text/html')
 		self.end_headers()
 		# Send the html message
-		self.wfile.write("Hello OKD!")
+		self.wfile.write("Hello World!")
 		return
 
 try:
